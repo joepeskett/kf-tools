19c7a8ff191449e2441ef118484ac47eb0fd116d
joepeskett <jwpeskett@gmail.com>
2020-05-22 21:36:59 +0100

    update package details and remove extra bits

---
+++ R/kubeflowTool.R
@@ -0,0 +1,9 @@
+#' @description
+#' kfTool is for building R components from R functions.
+#' @keywords internal
+#' @import rlang
+#' @importFrom glue glue glue_collapse glue_data
+#' @importFrom stats setNames update
+#' @importFrom utils head tail
+#' @importFrom methods is
+"_PACKAGE"
